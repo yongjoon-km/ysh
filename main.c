2cef900061cf5fdb8b60b6e0c93033d400283281
jokingmoony <penubokim@gmail.com>
2023-01-24 22:40:38 +0900

    Create basic loop of shell

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ysh_loop();
 
 int main(int argc, char** argv) {
 
-    printf("hello world");
-
+    ysh_loop();
     return 0;
 }
+
+void ysh_loop() {
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t size;
+    while ((size = getline(&line, &len, stdin)) != -1) {
+        if (strcmp(line, "exit\n") == 0) {
+            break;
+        }
+        printf("%s\n", line);
+    }
+    return;
+}
+
